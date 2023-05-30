28e0928279513ea0d77e483e6744cef48af07a66
Luca Esposito <luca.esposito.98@outlook.it>
2023-05-30 18:02:41 +0200

    feat:footer

---
+++ scripts/carouselSocial.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $("#owl-demo").owlCarousel({
+   
+        autoplay: true, 
+        autoplayTimeout: 3000,
+        items : 7,
+        loop: true,
+        ltr: true,
+        itemsDesktop : [1199,3],
+        itemsDesktopSmall : [979,3],
+        margin: 10,
+        dots: false,
+    });
+   
+  });
